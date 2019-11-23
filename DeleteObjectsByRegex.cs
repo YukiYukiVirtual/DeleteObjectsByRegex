71de919ce4dc4a194fa95c12a4fb100a516a310b
YukiYukiVirtual <37247312+YukiYukiVirtual@users.noreply.github.com>
2019-11-23 14:00:36 +0900

    release

---
+++ DeleteObjectsByRegex.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+
+public class DeleteObjectsByRegex : EditorWindow
+{
+	private static GameObject parentObject;
+	private static string pattern;
+	
+	// ヒエラルキーのオブジェクト右クリックで使えるやつ
+	[MenuItem ("GameObject/Delete Objects By Regex", false, 21)]
+	public static void ShowWindow () {
+		parentObject = Selection.activeGameObject;
+		EditorWindow.GetWindow (typeof (DeleteObjectsByRegex));
+	}
+	
+	// 出てくるウィンドウ
+	private void OnGUI()
+	{
+		EditorGUILayout.BeginVertical(GUI.skin.box);
+		EditorGUILayout.LabelField("すべての子要素を正規表現で削除します。");
+		GUILayout.Space(5);
+		EditorGUILayout.BeginHorizontal();
+		parentObject = (GameObject)EditorGUILayout.ObjectField("Parent", parentObject, typeof(GameObject), true);
+		EditorGUILayout.EndHorizontal();
+		EditorGUILayout.BeginHorizontal();
+		pattern = EditorGUILayout.TextField("pattern", pattern);
+		EditorGUILayout.EndHorizontal();
+
+		EditorGUILayout.EndVertical();
+
+		if (GUILayout.Button("正規表現で削除する"))
+		{
+			exec();
+		}
+	}
+
+	// 削除処理
+	private void exec()
+	{
+		StringBuilder  sb = new StringBuilder("Deleted Objects:\n");
+		
+		Regex regex = new Regex (pattern);
+		
+		// 対象要素を列挙する
+		List<GameObject> children = new List<GameObject> ();
+		GetChildren (parentObject, ref children, ref regex);
+		
+		// 対象要素をたどる
+		foreach(GameObject child in children)
+		{
+			sb.AppendFormat("{0}\n", child.name);
+			// Undoできるように削除する
+			Undo.DestroyObjectImmediate(child);
+		}
+
+		EditorUtility.DisplayDialog("完了", sb.ToString(), "OK");
+
+		return;
+	}
+
+	public static void GetChildren (GameObject obj, ref List<GameObject> list, ref Regex regex)
+	{
+		// 子要素を取得する
+		Transform children = obj.GetComponentInChildren<Transform> ();
+		foreach (Transform child in children) {
+			// 正規表現マッチでリストに追加する
+			if(regex.IsMatch(child.gameObject.name))
+			{
+				list.Add (child.gameObject);
+			}
+			GetChildren (child.gameObject, ref list, ref regex);
+		}
+	}
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Delete Objects By Regex
+
+## 使い方
+ヒエラルキーのオブジェクトを右クリックで「Delete Objects By Regex」が追加されます。
+正規表現にマッチする子孫オブジェクトをすべて削除します。
+間違えたらUndoできます。
+
+## インストール
+DeleteObjectsByRegex.csを適当な場所に保存します。
+https://yukiyukivirtual.booth.pm
+にあるunitypackageを使ってもいいです。
+
+## License
+Copyright (c) 2019 YukiYukiVirtual
+Released under the MIT license
+https://opensource.org/licenses/mit-license.php
